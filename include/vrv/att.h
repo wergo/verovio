d9ac0e9cd2c4a02e771b981dd9c67c2e30346cab
Laurent Pugin <lxpugin@gmail.com>
2015-03-27 09:42:25 +0100

    Splitting converter files for preparing ODD output

--- include/vrv/att.h
+++ include/vrv/att.h
@@ -102,87 +102,9 @@ public:
     
     /** @name Converters for writing and reading */
     ///@{
-    std::string AccidentalExplicitToStr(data_ACCIDENTAL_EXPLICIT data);
-    data_ACCIDENTAL_EXPLICIT StrToAccidentalExplicit(std::string value);
-    
-    std::string AccidentalImplicitToStr(data_ACCIDENTAL_IMPLICIT data);
-    data_ACCIDENTAL_IMPLICIT StrToAccidentalImplicit(std::string value);
-    
-    std::string BarRenditionToStr(data_BARRENDITION data);
-    data_BARRENDITION StrToBarRendition(std::string value);
-    
-    std::string BoolToStr(data_BOOLEAN data);
-    data_BOOLEAN StrToBool(std::string value);
-    
-    std::string ClefShapeToStr(data_CLEFSHAPE data);
-    data_CLEFSHAPE StrToClefShape(std::string value);
-    
-    std::string ConToStr(data_CON data);
-    data_CON StrToCon(std::string value);
-    
-    std::string DurToStr(data_DURATION data);
-    data_DURATION StrToDur(std::string value);
-    
-    std::string FontstyleToStr(data_FONTSTYLE data);
-    data_FONTSTYLE StrToFontstyle(std::string value);
-    
-    std::string FontweightToStr(data_FONTWEIGHT data);
-    data_FONTWEIGHT StrToFontweight(std::string value);
-        
-    std::string KeySignatureToStr(data_KEYSIGNATURE data);
-    data_KEYSIGNATURE StrToKeySignature(std::string value);
-    
-    std::string LigatureToStr(data_LIGATURE data);
-    data_LIGATURE StrToLigature(std::string value);
-    
-    std::string MensurationSignToStr(data_MENSURATIONSIGN data);
-    data_MENSURATIONSIGN StrToMensurationSign(std::string value);
-    
-    std::string MeterSignToStr(data_METERSIGN data);
-    data_METERSIGN StrToMeterSign(std::string value);
-    
-    std::string ModeToStr(data_MODE data);
-    data_MODE StrToMode(std::string value);
 
-    std::string ModusmaiorToStr(data_MODUSMAIOR data);
-    data_MODUSMAIOR StrToModusmaior(std::string value);
+    #include "att_converters.h"
     
-    std::string ModusminorToStr(data_MODUSMINOR data);
-    data_MODUSMINOR StrToModusminor(std::string value);
-    
-    std::string OctaveDisToStr(data_OCTAVE_DIS data);
-    data_OCTAVE_DIS StrToOctaveDis(std::string value);
-    
-    std::string OrientationToStr(data_ORIENTATION data);
-    data_ORIENTATION StrToOrientation(std::string value);
-    
-    std::string PitchNameToStr(data_PITCHNAME data);
-    data_PITCHNAME StrToPitchName(std::string value);
-    
-    std::string PlaceToStr(data_PLACE data);
-    data_PLACE StrToPlace(std::string value);
-    
-    std::string ProlatioToStr(data_PROLATIO data);
-    data_PROLATIO StrToProlatio(std::string value);
-    
-    std::string StemDirectionToStr(data_STEMDIRECTION data);
-    data_STEMDIRECTION StrToStemDirection(std::string value);
-    
-    std::string StemPositionToStr(data_STEMPOSITION data);
-    data_STEMPOSITION StrToStemPosition(std::string value);
-    
-    std::string TempusToStr(data_TEMPUS data);
-    data_TEMPUS StrToTempus(std::string value);
-    
-    std::string TieToStr(data_TIE data);
-    data_TIE StrToTie(std::string value);
-    
-    std::string Tstamp2ToStr(typedef_TSTAMP2 data);
-    typedef_TSTAMP2 StrToTstamp2(std::string value);
-    
-    std::string WordPosToStr(data_WORDPOS data);
-    data_WORDPOS StrToWordPos(std::string value);
-  
     ///@}
 
 };
